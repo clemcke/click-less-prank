173f79656368f6bc9798fd4644bfef4aebfde512
Chris Lemcke <sixtimesnine@gmail.com>
2012-08-23 12:58:01 -0700

    basic implementation for a sibnle link at a time

--- click_less.js
+++ click_less.js
@@ -5,19 +5,34 @@ $(document).ready(function(){
   });
 
   $('a').mouseenter(function(e){
-    $(".collision").show();
     var $this = $(this);
     replaceMe($this);
   });
 
-  $('a').mouseout(function(e){
-    $(".collision").hide();
-  })
-
-  $('a.showme').mouseenter(function(e){
-    var $this = $(this);
-  });
 });
+
+function replaceMe(el){
+  if($(".showme").length == 0){
+    el.parent().append(el.clone().addClass("showme"));
+
+    var $dup = $(".showme");
+
+    $dup.hover(function(e){
+      var $this = $(this);
+      moveOuttaTheWay(e,$this);
+    })
+
+    $dup.css("left",el.position()["left"]);
+    $dup.css("top",el.position()["top"]);
+    $dup.css("cursor","default");
+    $dup.click(function(){return false;});
+
+    el.css("opacity",0);
+    el.addClass("hideme");
+    el.click(function(){return false;});
+    el.css("cursor", 'default');
+  }
+}
 
 function getBox(el){
   var box = {};
@@ -25,18 +40,29 @@ function getBox(el){
   box['top'] = el.position()['top'];
   box['right'] = el.position()['left'] + el.width(); // plus border
   box['bottom'] = el.position()['top'] + el.height(); // plus border
+  box['center_x'] = (box['right'] + box['left'])/2.0;
+  box['center_y'] = (box['top'] + box['bottom'])/2.0;
+  box['width'] = box['right'] - box['left'];
+  box['height'] = box['bottom'] - box['top'];
   return box;
 }
 
-function replaceMe(el){
-  if($(".showme").length == 0){
-    el.parent().append(el.clone().addClass("showme"));
-    var $dup = $(".showme");
-    $dup.css("left",el.position()["left"]);
-    $dup.css("top",el.position()["top"]);
-    el.css("opacity",0);
-    el.addClass("hideme");
-    el.click(function(){return false;});
-    el.css("cursor", 'default');
+function moveOuttaTheWay(e,el){
+  var box = getBox(el),
+      x = e.pageX,
+      y = e.pageY;
+  if(x >= box.center_x){
+    if(x <= box.right)
+      el.css("left",x - box.width - 5);
+  }else{
+    if(x >= box.left)
+      el.css("left",x + 5);
+  }
+  if(y >= box.center_y){
+    if(y <= box.bottom)
+      el.css("top",y - box.height - 5);
+  }else{
+    if(y >= box.top)
+      el.css("top",y + 5);
   }
 }
